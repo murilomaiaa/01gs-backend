784f78488b88bad20c23792baad176ecb573c6b0
Murilo Maia <murilomaia.bb@gmail.com>
2020-06-20 15:45:35 -0300

    changing repository

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 // const { uuid } = require("uuidv4");
 
@@ -10,6 +10,17 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+
+function validateProjectId(req, res, next) {
+  const { id } = req.params;
+
+  if (!isUuid(id)) {
+    return res.status(400).json({ error: "Invalid repository Id" });
+  }
+
+  return next();
+}
+app.use("/repositories/:id", validateProjectId);
 
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
@@ -31,8 +42,29 @@ app.post("/repositories", (req, res) => {
   return res.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, url, techs } = req.body;
+
+  const repoIndex = repositories.findIndex((repository) => repository.id == id);
+
+  if (repoIndex < 0) {
+    return res.status(400).json({ error: "Id doesn't exists" });
+  }
+
+  const likes = repositories[repoIndex].likes;
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes,
+  };
+
+  repositories[repoIndex] = repository;
+
+  return res.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
